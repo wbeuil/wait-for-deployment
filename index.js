b42401a95459a7abe636be053aface76b34cc45f
William Beuil <william.beuil@gmail.com>
2020-11-11 21:43:59 +0100

    Add ncc and formating deps

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 const sleep = (seconds) =>
   new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 
 async function waitForDeployment() {
   const { eventName, payload, repo } = github.context;
-  const token = core.getInput("token");
-  const timeout = core.getInput("timeout") * 1000;
+  const token = core.getInput('token');
+  const timeout = parseInt(core.getInput('timeout'), 10) * 1000;
   const endTime = new Date().getTime() + timeout;
 
   let params = {
@@ -16,17 +16,17 @@ async function waitForDeployment() {
 
   core.debug(`eventName? ${eventName}`);
 
-  if (eventName === "pull_request") {
+  if (eventName === 'pull_request') {
     params = {
       ...params,
       sha: payload.pull_request.head.sha,
-      environment: "Preview",
+      environment: 'Preview',
     };
-  } else if (eventName === "push") {
+  } else if (eventName === 'push') {
     params = {
       ...params,
       sha: payload.head_commit.id,
-      environment: "Production",
+      environment: 'Production',
     };
   } else {
     throw new Error(`Unhandled event: ${eventName}`);
@@ -42,7 +42,7 @@ async function waitForDeployment() {
 
       if (deployments.length > 1) {
         throw new Error(
-          `There should be only one deployment for ${params.sha} but found ${deployments.length} instead.`
+          `There should be only one deployment for ${params.sha} but found ${deployments.length} instead.`,
         );
       }
 
@@ -53,7 +53,7 @@ async function waitForDeployment() {
         });
 
         const [success] = statuses.filter(
-          (status) => status.state === "success"
+          (status) => status.state === 'success',
         );
 
         if (success) {
@@ -70,14 +70,14 @@ async function waitForDeployment() {
   }
 
   throw new Error(
-    `Timeout reached before deployment for ${params.sha} was found.`
+    `Timeout reached before deployment for ${params.sha} was found.`,
   );
 }
 
 (async () => {
   try {
     const url = await waitForDeployment();
-    core.setOutput("url", url);
+    core.setOutput('url', url);
   } catch (err) {
     core.setFailed(err.message);
   }
